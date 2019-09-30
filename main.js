d4f8a97d0e37197b9265d1b633e0a7c210999daf
Kirk <kkchu791@gmail.com>
2019-09-30 15:50:06 -0700

    add rest of the logic

--- main.js
+++ main.js
@@ -1,11 +1,58 @@
 
 function startGame() {
   document.turn = "X";
+  document.gameOver = false;
 }
 
 function nextMove(square) {
-  square.innerText = document.turn;
+  if (document.gameOver === false) {
+    if (taken(square)) {
+      return message("That spot is taken.")
+    } else {
+      setPiece(square);
+
+    }
+  }
+
+  if (playable()) {
+    switchPlayers()
+  } else {
+    document.gameOver = true
+    if (hasWinner()) {
+      message(`Player ${document.turn} wins!`)
+    } else {
+      message(`It's a draw!`)
+    }
+  }
+}
+
+function switchPlayers() {
   switchTurn();
+  message(`Player ${document.turn} turn!`)
+}
+
+function setPiece(square) {
+  square.innerText = document.turn;
+}
+
+function message(msg) {
+  return document.getElementsByClassName("message")[0].innerText = msg
+}
+
+function taken(square) {
+  return square.innerText != ""
+}
+
+function playable() {
+  return hasWinner() === false && board_has_empty_slots()
+}
+
+function board_has_empty_slots() {
+  const slots = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+  return slots.some(num => {
+    return document.getElementById(num).innerText === ""
+  })
 }
 
 function switchTurn() {
@@ -16,13 +63,47 @@ function switchTurn() {
   }
 }
 
-function reset() {
-  var el = document.getElementsByClassName("square");
-  // go through all elements and set the innerHTML to an empty string
+function hasWinner() {
+  const sq_1 = document.getElementById("1").innerText
+  const sq_2 = document.getElementById("2").innerText
+  const sq_3 = document.getElementById("3").innerText
 
-  for (var i = 0; i < el.length; i++) {
-    console.log(el[i])
-    el[i].innerText = "";
+  const sq_4 = document.getElementById("4").innerText
+  const sq_5 = document.getElementById("5").innerText
+  const sq_6 = document.getElementById("6").innerText
+
+  const sq_7 = document.getElementById("7").innerText
+  const sq_8 = document.getElementById("8").innerText
+  const sq_9 = document.getElementById("9").innerText
+
+
+  return checkRow(sq_1, sq_2, sq_3) ||
+         checkRow(sq_4, sq_5, sq_6) ||
+         checkRow(sq_7, sq_8, sq_9) ||
+
+         // horizontal check
+
+         checkRow(sq_1, sq_4, sq_7) ||
+         checkRow(sq_2, sq_5, sq_8) ||
+         checkRow(sq_3, sq_6, sq_9) ||
+
+
+         //diagonal checkRow
+         checkRow(sq_1, sq_5, sq_9) ||
+         checkRow(sq_3, sq_5, sq_7);
+}
+
+
+function checkRow(a, b, c) {
+  return a === b && b === c && a == document.turn;
+}
+
+function reset() {
+  var squares = document.getElementsByClassName("square");
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].innerText = "";
   }
+  document.gameOver = false
+  document.getElementsByClassName("message")[0].innerText = `Player ${document.turn} turn!`
 
 }
